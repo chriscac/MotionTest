2fed024c99e7f238068f87bf0b3d384e62bba41a
Chris Cacioppe <chris.cacioppe@gmail.com>
2025-06-10 18:56:44 -0700

    Initial Commit

---
+++ MotionTest/MotionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionTestApp.swift
+//  MotionTest
+//
+//  Created by Chris Cacioppe on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MotionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotionTestTests/MotionTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  MotionTestTests.swift
+//  MotionTestTests
+//
+//  Created by Chris Cacioppe on 6/10/25.
+//
+
+import Testing
+
+struct MotionTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
